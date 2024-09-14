1ecd389e414ccbbca067bffe9e5def5c6b318962
Vaibhav kumar <159615855+VaibhavTechWizard@users.noreply.github.com>
2024-09-14 07:37:26 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3+4)// We are using node js not browser
+
+// console.log(3+3);
+
+
+let name="vaibhav"
+let age=21
+let isLoggedIn= false
+let state;
+// number=> 2 to power 53
+// bigInt
+// string => "  "
+// boolean => true/false
+// null => standalone value
+// Undefined => value not assinged yet
+// symbol => unique
+
+//object
+
+console.log(typeof null);      // Object
+console.log(typeof undefined); // unidefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=1234;
+let accountEmail="vaibhav@123"
+var accountPassword="123456"
+accountCity="Shimla"
+
+// accountId = 2
+accountEmail="hero@123"
+accountPassword="1234"
+accountCity="Jaipur"
+// console.log(accountId);
+let accountState;
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
