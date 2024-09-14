8ce52a66b6d5493514bf9f307dfacf37f415a66c
Vaibhav kumar <159615855+VaibhavTechWizard@users.noreply.github.com>
2024-09-14 10:09:27 +0000

    conversion

---
+++ 01_basics/04_conversionOperator.js
@@ -0,0 +1,27 @@
+// let score=undefined//NaN
+let score=true
+
+// console.log(typeof score);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);//NaN
+
+//"33 => 33
+// "33abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);//true
+
+// 1 => true ; 0 => false
+// "" => false
+// "vaibhav" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
+
