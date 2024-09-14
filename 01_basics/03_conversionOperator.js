7bede3211f8940baafeeb43219cd312d31b620ea
Vaibhav kumar <159615855+VaibhavTechWizard@users.noreply.github.com>
2024-09-14 10:33:22 +0000

    Comparison

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,63 @@
+// let score=undefined//NaN
+let score=true
+
+// console.log(typeof score);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);//NaN
+
+//"33 => 33
+// "33abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);//true
+
+// 1 => true ; 0 => false
+// "" => false
+// "vaibhav" => true
+
+
+let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log( typeof stringNumber);
+
+
+
+// ********************  Operations **********************
+
+let value =3
+let negValue=-value
+// console.log(negValue);
+
+
+let str1="hello"
+let str2="hey"
+
+let str3=str1+str2
+// console.log(str1+str2);
+
+
+// console.log(1+2);
+// console.log(1+"2");//12
+// console.log("1"+2);//12
+// console.log(1-"2");//-1
+// console.log("1"-2);//-1
+
+
+// console.log(1+"2"+1);//122
+// console.log("1"+2+1);//122=> If string in first it converts all into string
+// console.log(1+1+"2");//22 => If string in last then first arithmetic operation has to done
+
+// console.log(+true);//1
+// console.log(+"");  //0 
+
+let n1,n2,n3
+n1=n2=n3=2+2
+
+let game = 100
+game++;
+console.log(game);//101
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>"1");//true
+// console.log("02" > "1");//false
+
+// console.log(null==0);//false
+// console.log(null>0);//true null converted to 0
+// console.log(null<0);//false
+
+// console.log(undefined==0);//all false
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+
+// === => It checks datatypes and also converts
+// console.log("2"===2);//false
