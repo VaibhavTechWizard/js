22fcca5f208e37dc7b57e4127290d9b3f37931a7
Vaibhav kumar <159615855+VaibhavTechWizard@users.noreply.github.com>
2024-09-14 18:06:44 +0000

    06.string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,11 @@
+const name = "vaibhav"
+const repoCount = 50
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hithesj')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
