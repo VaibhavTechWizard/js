4d8e60b61a4bfee545cbfe326222dc27a3e29fed
Vaibhav kumar <159615855+VaibhavTechWizard@users.noreply.github.com>
2024-09-15 06:12:02 +0000

    01_basics/07_num_and_maths

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,37 @@
+const score=400;
+// console.log(score);
+
+// const balance= new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber=1123.2345
+// console.log(otherNumber.toPrecision(5));
+
+// const hundreds=100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ******************* Math ********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(-4.51)); //-5
+// console.log(Math.floor(4.9));   //4
+// console.log(Math.max(1,2,3,4,6));    //6
+// console.log(Math.min(-1,0,1,3,5));//-1
+
+console.log(Math.random());//Random value lies between 0 to 1
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
